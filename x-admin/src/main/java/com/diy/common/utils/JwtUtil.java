ac2207ac54c822af697f3e7b320eeb0821c0b6c8
youzi <youzi1850@gmail.com>
2025-11-10 20:29:25 +0800

    jwtutil修改

--- x-admin/src/main/java/com/diy/common/utils/JwtUtil.java
+++ x-admin/src/main/java/com/diy/common/utils/JwtUtil.java
@@ -5,10 +5,12 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Date;
 import java.util.UUID;
@@ -26,36 +28,36 @@ public class JwtUtil {
         // 过期时间
         long expTime = currentTime+JWT_EXPIRE;
         // 构建jwt
-        JwtBuilder builder = Jwts.builder()
-                .setId(UUID.randomUUID()+"")
-                .setSubject(JSON.toJSONString(data))
-                .setIssuer("system")
-                .setIssuedAt(new Date(currentTime))
-                .signWith(SignatureAlgorithm.HS256, encodeSecret(JWT_KEY))
-                .setExpiration(new Date(expTime));
-        return builder.compact();
+        return Jwts.builder()
+                .id(UUID.randomUUID().toString())
+                .subject(JSON.toJSONString(data))
+                .issuer("system")
+                .issuedAt(new Date(currentTime))
+                .expiration(new Date(expTime))
+                .compact();
     }
 
-    private SecretKey encodeSecret(String key){
-        byte[] encode = Base64.getEncoder().encode(key.getBytes());
-        SecretKeySpec aes = new SecretKeySpec(encode, 0, encode.length, "AES");
-        return  aes;
+//    private SecretKey encodeSecret(String key){
+//        byte[] encode = Base64.getEncoder().encode(key.getBytes());
+//        SecretKeySpec aes = new SecretKeySpec(encode, 0, encode.length, "AES");
+//        return  aes;
+//    }
+    private SecretKey generateKey() {
+        byte[] keyBytes = JWT_KEY.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 
     public  Claims parseToken(String token){
-        Claims body = Jwts.parser()
-                .setSigningKey(encodeSecret(JWT_KEY))
-                .parseClaimsJws(token)
-                .getBody();
-        return body;
+        return Jwts.parser()
+                .verifyWith(generateKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     public <T> T parseToken(String token,Class<T> clazz){
-        Claims body = Jwts.parser()
-                .setSigningKey(encodeSecret(JWT_KEY))
-                .parseClaimsJws(token)
-                .getBody();
-        return JSON.parseObject(body.getSubject(),clazz);
+        Claims claims = parseToken(token);
+        return JSON.parseObject(claims.getSubject(), clazz);
     }
 
 }
